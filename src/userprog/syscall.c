bb222de3053ee0dfd6c6eddeae10088ea03ebb3e
KalinduGandara <kalindugandara16@gmail.com>
2021-12-07 22:39:41 +0530

    refactor 28 fail

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -18,25 +18,23 @@
 #define STDERR 2
 
 struct lock memory_lock;
-static int fd_num=2;
-
+static int fd_num = 2;
 
 static void syscall_handler(struct intr_frame *);
 
-static bool put_user (uint8_t *udst, uint8_t byte);
+static bool put_user(uint8_t *udst, uint8_t byte);
 static int get_user(const uint8_t *uaddr);
 int read_mem(void *dest, void *src, size_t size);
-void check_memory(void * addr, size_t size);
+void check_memory(void *addr, size_t size);
 bool check_validate(void *addr);
 
-void sys_write(int fd_, void * buffer, int size, struct intr_frame *f);
-void sys_read(int fd_, void * buffer, int size, struct intr_frame *f);
+void sys_write(int fd_, void *buffer, int size, struct intr_frame *f);
+void sys_read(int fd_, void *buffer, int size, struct intr_frame *f);
 void sys_exec(char *cmd, struct intr_frame *f);
 void sys_wait(int tid, struct intr_frame *f);
 void sys_create(char *name, size_t size, struct intr_frame *f);
-void sys_open(char * name, struct intr_frame *f);
+void sys_open(char *name, struct intr_frame *f);
 void sys_close(int fd_, struct intr_frame *f);
-
 
 void syscall_init(void)
 {
@@ -58,10 +56,10 @@ syscall_handler(struct intr_frame *f)
   {
     int fd, size;
     void *buffer;
-    read_mem(&fd,esp+4,sizeof(fd));
-    read_mem(&buffer, esp+8, sizeof(fd));
-    read_mem(&size, esp+12, sizeof(fd));
-    sys_read(fd,buffer,size,f);
+    read_mem(&fd, esp + 4, sizeof(fd));
+    read_mem(&buffer, esp + 8, sizeof(fd));
+    read_mem(&size, esp + 12, sizeof(fd));
+    sys_read(fd, buffer, size, f);
     break;
   }
   case SYS_EXIT:
@@ -75,10 +73,10 @@ syscall_handler(struct intr_frame *f)
   {
     int fd, size;
     void *buffer;
-    read_mem(&fd,esp+4,sizeof(fd));
-    read_mem(&buffer, esp+8, sizeof(buffer));
-    read_mem(&size, esp+12, sizeof(size));
-    sys_write(fd,buffer,size,f);
+    read_mem(&fd, esp + 4, sizeof(fd));
+    read_mem(&buffer, esp + 8, sizeof(buffer));
+    read_mem(&size, esp + 12, sizeof(size));
+    sys_write(fd, buffer, size, f);
     break;
   }
   case SYS_HALT:
@@ -88,56 +86,64 @@ syscall_handler(struct intr_frame *f)
   }
   case SYS_EXEC:
   {
-    void * cmd;
-    read_mem(&cmd, esp+4, sizeof(cmd));
-    sys_exec(cmd,f);
+    void *cmd;
+    read_mem(&cmd, esp + 4, sizeof(cmd));
+    sys_exec(cmd, f);
     break;
   }
   case SYS_WAIT:
   {
     int tid;
-    read_mem(&tid,esp+4,sizeof(tid));
-    sys_wait(tid,f);
+    read_mem(&tid, esp + 4, sizeof(tid));
+    sys_wait(tid, f);
     break;
   }
-  case SYS_CREATE: 
+  case SYS_CREATE:
   {
-    char * name;
+    char *name;
     size_t size;
-    read_mem(&name, esp+4, sizeof(name));
-    read_mem(&size, esp+8, sizeof(size));
-    sys_create(name,size,f);
+    read_mem(&name, esp + 4, sizeof(name));
+    read_mem(&size, esp + 8, sizeof(size));
+    sys_create(name, size, f);
     break;
   }
   case SYS_OPEN:
   {
     char *name;
-    read_mem(&name, esp+4, sizeof(name));
-    sys_open(name,f);
+    read_mem(&name, esp + 4, sizeof(name));
+    sys_open(name, f);
     break;
   }
   case SYS_CLOSE:
   {
     int fd;
-    read_mem(&fd, esp+4, sizeof(fd));
-    sys_close(fd,f);
+    read_mem(&fd, esp + 4, sizeof(fd));
+    sys_close(fd, f);
     break;
   }
-  case SYS_REMOVE:{}
-  case SYS_FILESIZE:{}
-  case SYS_SEEK:{}
-  case SYS_TELL:{}
+  case SYS_REMOVE:
+  {
+  }
+  case SYS_FILESIZE:
+  {
+  }
+  case SYS_SEEK:
+  {
+  }
+  case SYS_TELL:
+  {
+  }
   default:
-    {
-      int exit_code;
-      read_mem(&exit_code, esp+4,sizeof(exit_code));
-      sys_exit(exit_code,NULL);
-    }
+  {
+    int exit_code;
+    read_mem(&exit_code, esp + 4, sizeof(exit_code));
+    sys_exit(exit_code, NULL);
+  }
   }
 }
 void sys_exit(int exit_code, struct intr_frame *f UNUSED)
 {
-  struct tcb * tcb = thread_current()->tcb;
+  struct tcb *tcb = thread_current()->tcb;
   printf("%s: exit(%d)\n", thread_current()->name, exit_code);
   if (tcb)
   {
@@ -147,165 +153,159 @@ void sys_exit(int exit_code, struct intr_frame *f UNUSED)
   thread_exit();
 }
 
-void
-sys_exec(char *cmd, struct intr_frame *f)
+void sys_exec(char *cmd, struct intr_frame *f)
 {
-  check_memory(cmd,sizeof(cmd));
+  check_memory(cmd, sizeof(cmd));
   lock_acquire(&memory_lock);
-  f->eax = process_execute((const char*)cmd);
-  struct thread * cur = thread_current();
+  f->eax = process_execute((const char *)cmd);
+  struct thread *cur = thread_current();
   lock_release(&memory_lock);
 }
-void 
-sys_wait(int tid, struct intr_frame *f)
+void sys_wait(int tid, struct intr_frame *f)
 {
   f->eax = process_wait(tid);
 }
-void
-sys_create(char *name, size_t size, struct intr_frame *f)
+void sys_create(char *name, size_t size, struct intr_frame *f)
 {
-  check_memory(name,sizeof(name));
+  check_memory(name, sizeof(name));
   lock_acquire(&memory_lock);
-  f->eax = filesys_create((const char*)name,size);
+  f->eax = filesys_create((const char *)name, size);
   lock_release(&memory_lock);
 }
-void
-sys_open(char * name, struct intr_frame *f)
+void sys_open(char *name, struct intr_frame *f)
 {
-  
-  struct file * open=NULL;
-  struct filedesc * fd;
-  check_memory(name,sizeof(name));
+
+  struct file *open = NULL;
+  struct filedesc *fd;
+  check_memory(name, sizeof(name));
   lock_acquire(&memory_lock);
   fd = palloc_get_page(0);
-  if(fd == NULL)
+  if (fd == NULL)
   {
     palloc_free_page(fd);
-    goto malicious_ending;
+    f->eax = -1;
+    lock_release(&memory_lock);
   }
   else
   {
     open = filesys_open(name);
-    if(open == NULL)
-      goto malicious_ending;
-    fd->f = open;  
+    if (open == NULL)
+    {
+      f->eax = -1;
+      lock_release(&memory_lock);
+    }
+    fd->f = open;
     fd->fd_num = ++fd_num;
     fd->master = thread_current();
-    list_push_back(&(thread_current()->file_list),&(fd->elem));
-    f->eax=fd->fd_num;
+    list_push_back(&(thread_current()->file_list), &(fd->elem));
+    f->eax = fd->fd_num;
     lock_release(&memory_lock);
     return;
   }
-  malicious_ending:
-  f->eax=-1;
-  lock_release(&memory_lock);
 }
-void
-sys_close(int fd_, struct intr_frame *f UNUSED)
+void sys_close(int fd_, struct intr_frame *f UNUSED)
 {
-    struct list_elem *tmp;
-    struct filedesc *fd;
-    if(!list_empty(&thread_current()->file_list))
+  struct list_elem *tmp;
+  struct filedesc *fd;
+  if (!list_empty(&thread_current()->file_list))
+  {
+    lock_acquire(&memory_lock);
+    for (tmp = list_front(&thread_current()->file_list);; tmp = list_next(tmp))
     {
-      lock_acquire(&memory_lock);
-      for(tmp=list_front(&thread_current()->file_list); ; tmp=list_next(tmp))
-      {
-        fd = list_entry(tmp, struct filedesc, elem);
-        if(fd->fd_num == fd_)
-         break;
-        if(tmp == list_tail(&thread_current()->file_list) && (fd->fd_num != fd_))
-        {
-          lock_release(&memory_lock);
-          return;
-        }
-      }
-      if(thread_current()->tid == fd->master->tid) // check master thread.
+      fd = list_entry(tmp, struct filedesc, elem);
+      if (fd->fd_num == fd_)
+        break;
+      if (tmp == list_tail(&thread_current()->file_list) && (fd->fd_num != fd_))
       {
         lock_release(&memory_lock);
-        file_close(fd->f);
-        list_remove(&(fd->elem));
-        palloc_free_page(fd);
+        return;
       }
     }
-} 
-void 
-sys_write(int fd_, void * buffer, int size, struct intr_frame *f)
+    if (thread_current()->tid == fd->master->tid) // check master thread.
+    {
+      lock_release(&memory_lock);
+      file_close(fd->f);
+      list_remove(&(fd->elem));
+      palloc_free_page(fd);
+    }
+  }
+}
+void sys_write(int fd_, void *buffer, int size, struct intr_frame *f)
 {
-  if(buffer == NULL)
-    sys_exit(-1,NULL);
-  check_memory(buffer,sizeof(buffer));
-  if(!check_validate(buffer) || !check_validate(buffer+size))
-    sys_exit(-1,NULL);
-  
+  if (buffer == NULL)
+    sys_exit(-1, NULL);
+  check_memory(buffer, sizeof(buffer));
+  if (!check_validate(buffer) || !check_validate(buffer + size))
+    sys_exit(-1, NULL);
+
   lock_acquire(&memory_lock);
-  if(fd_ == STDOUT)
+  if (fd_ == STDOUT)
   {
     putbuf(buffer, size);
     lock_release(&memory_lock);
-    f->eax=size;
+    f->eax = size;
   }
-  else if(fd_ == STDIN)
+  else if (fd_ == STDIN)
   {
-    f->eax=-1;
+    f->eax = -1;
     lock_release(&memory_lock);
     return;
   }
   else
   {
     struct list_elem *tmp;
-    struct filedesc * fd=NULL;
-    if(!list_empty(&thread_current()->file_list))
+    struct filedesc *fd = NULL;
+    if (!list_empty(&thread_current()->file_list))
     {
-      for(tmp=list_front(&thread_current()->file_list); ; tmp=list_next(tmp))
+      for (tmp = list_front(&thread_current()->file_list);; tmp = list_next(tmp))
       {
         fd = list_entry(tmp, struct filedesc, elem);
-        if(fd->fd_num == fd_)
-         break;
-        if(tmp == list_tail(&thread_current()->file_list) && (fd->fd_num != fd_))
+        if (fd->fd_num == fd_)
+          break;
+        if (tmp == list_tail(&thread_current()->file_list) && (fd->fd_num != fd_))
         {
           lock_release(&memory_lock);
-          f->eax=-1;
+          f->eax = -1;
           return;
         }
       }
     }
-    if(fd !=NULL)
+    if (fd != NULL)
     {
-      f->eax = file_write(fd->f,buffer,size);
+      f->eax = file_write(fd->f, buffer, size);
       lock_release(&memory_lock);
       return;
     }
     else
     {
       lock_release(&memory_lock);
-      f->eax=-1;
+      f->eax = -1;
       return;
     }
   }
 }
-void
-sys_read(int fd_, void * buffer, int size, struct intr_frame *f)
+void sys_read(int fd_, void *buffer, int size, struct intr_frame *f)
 {
   unsigned i;
-  check_memory(buffer,sizeof(buffer));
-  check_memory(buffer,sizeof(buffer)+size-1);
+  check_memory(buffer, sizeof(buffer));
+  check_memory(buffer, sizeof(buffer) + size - 1);
   lock_acquire(&memory_lock);
-  if(fd_ <= 2 || fd_ > fd_num)
+  if (fd_ <= 2 || fd_ > fd_num)
   {
-    f->eax=-1;
+    f->eax = -1;
     lock_release(&memory_lock);
     return;
   }
-  if(fd_==STDIN) 
+  if (fd_ == STDIN)
   {
-    for(i=0; i<(unsigned)size; i++)
-        put_user((unsigned char *)(buffer+i),input_getc());
+    for (i = 0; i < (unsigned)size; i++)
+      put_user((unsigned char *)(buffer + i), input_getc());
     lock_release(&memory_lock);
     f->eax = size;
   }
-  else if(fd_ == STDOUT)
+  else if (fd_ == STDOUT)
   {
-    f->eax=-1;
+    f->eax = -1;
     lock_release(&memory_lock);
     return;
   }
@@ -313,25 +313,25 @@ sys_read(int fd_, void * buffer, int size, struct intr_frame *f)
   {
     struct list_elem *tmp;
     struct filedesc *fd;
-    if(!list_empty(&thread_current()->file_list))
+    if (!list_empty(&thread_current()->file_list))
     {
-      for(tmp=list_front(&thread_current()->file_list); ; tmp=list_next(tmp))
+      for (tmp = list_front(&thread_current()->file_list);; tmp = list_next(tmp))
       {
         fd = list_entry(tmp, struct filedesc, elem);
-        if(fd->fd_num == fd_)
-         break;
-        if(tmp == list_tail(&thread_current()->file_list) && (fd->fd_num != fd_))
+        if (fd->fd_num == fd_)
+          break;
+        if (tmp == list_tail(&thread_current()->file_list) && (fd->fd_num != fd_))
         {
           lock_release(&memory_lock);
-          f->eax=-1;
+          f->eax = -1;
           return;
         }
       }
-      f->eax = file_read(fd->f,buffer,size);
+      f->eax = file_read(fd->f, buffer, size);
       lock_release(&memory_lock);
-      return ;
+      return;
     }
-    lock_release(&memory_lock);    
+    lock_release(&memory_lock);
   }
 }
 static int
@@ -339,31 +339,32 @@ get_user(const uint8_t *uaddr)
 {
   if (check_validate)
   {
-  int result;
-  asm("movl $1f, %0; movzbl %1, %0; 1:"
-      : "=&a"(result)
-      : "m"(*uaddr));
-  return result;
+    int result;
+    asm("movl $1f, %0; movzbl %1, %0; 1:"
+        : "=&a"(result)
+        : "m"(*uaddr));
+    return result;
   }
-  sys_exit(-1,NULL);
+  sys_exit(-1, NULL);
 }
 static bool
-put_user (uint8_t *udst, uint8_t byte)
+put_user(uint8_t *udst, uint8_t byte)
 {
   if (check_validate)
   {
     int error_code;
-    asm ("movl $1f, %0; movb %b2, %1; 1:"
-    : "=&a" (error_code), "=m" (*udst) : "q" (byte));
+    asm("movl $1f, %0; movb %b2, %1; 1:"
+        : "=&a"(error_code), "=m"(*udst)
+        : "q"(byte));
     return error_code != -1;
   }
-  sys_exit(-1,NULL);
+  sys_exit(-1, NULL);
 }
 int read_mem(void *dest, void *src, size_t size)
 {
   size_t i;
   int value;
-  check_memory(src,size);
+  check_memory(src, size);
   for (i = 0; i < size; i++)
   {
     value = get_user(src + i);
@@ -373,22 +374,24 @@ int read_mem(void *dest, void *src, size_t size)
   }
   return value;
 }
-void check_memory(void * addr, size_t size)
+void check_memory(void *addr, size_t size)
 {
   unsigned i;
   unsigned char *_cmd = addr;
-  for(i=0; i<size; i++)
+  for (i = 0; i < size; i++)
   {
-    if(!check_validate((void *)(_cmd+i)))
-      sys_exit(-1,NULL);
+    if (!check_validate((void *)(_cmd + i)))
+      sys_exit(-1, NULL);
   }
 }
 bool check_validate(void *addr)
 {
-  if((addr != NULL) && is_user_vaddr(addr))
+  if ((addr != NULL) && is_user_vaddr(addr))
   {
-    if((pagedir_get_page(thread_current()->pagedir, addr)) != NULL) return true;
-    else return false;
+    if ((pagedir_get_page(thread_current()->pagedir, addr)) != NULL)
+      return true;
+    else
+      return false;
   }
   return false;
 }
